49f7cfbaafe3276b8328d050a793c234775aef4f
Ryan <ryansaweczko@kingsds.network>
2021-01-15 14:33:35 -0500

    remove some unneeded files

--- 2020/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import string
-
-dictionary = pd.read_csv(r"C:\Users\cbyle\Desktop\Files\Pishing-Detection\Dictionary\top-34k.csv", engine= 'python') 
-word = dictionary['google.com']
-
-dictionary.tail()
