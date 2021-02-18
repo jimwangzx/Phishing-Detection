c89caf7f45250654e6f126729468367730813970
awebsters <17aw43@queensu.ca>
2021-02-18 18:50:08 -0500

    Basic version of server and chrome extension code

--- server/server.py
+++ server/server.py
@@ -18,18 +18,42 @@ def home_endpoint():
     return 'Welcome to the Phishing Detection Server!'
 
 
-@app.route('/predict', methods=['POST'])
-def get_prediction():
-    # Works only for a single sample
-
-    data = request.get_json()  # Get data posted as a json
-
-    # converts shape from (4,) to (1, 4)
+def predict(data):
     data = np.array(data)[np.newaxis, :]
+
     # runs globally loaded model on the data
     prediction = model.predict(data)
 
+    return prediction
+
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Works only for a single point
+
+    data = request.get_json()  # Get data posted as a json
+
+    prediction = predict(data)
+
     return str(prediction[0])
+
+
+@app.route('/multi-predict', methods=['POST'])
+def get_multiple_prediction():
+    # Works only for multiple points
+
+    predictions = []
+
+    data = request.get_json()  # Get data posted as a json
+    print(data)
+
+    for point in data:
+
+        prediction = predict(point)
+
+        predictions.append(str(prediction[0]))
+
+    return predictions
 
 
 if __name__ == '__main__':
